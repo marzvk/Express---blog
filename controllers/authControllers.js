e0c27daaf2313066fb7273c57124890b2ff81c09
marzvk <marianozovak1@gmail.com>
2025-10-24 19:12:01 -0300

    controllers register y login

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,23 +1,135 @@
+// Mostrar los formularios de registro y login desde ruta /auth.
+// Validar los datos enviados con bcrypt.
+// Crear usuarios nuevos.
+// Iniciar sesión con Passport.
+// Cerrar sesión.
 
 const User = require("../models/user");
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 
-exports.login_get = async (req, res, next) => {
-    res.send("not implemented get")
+// ===========================
+// MOSTRAR FORMULARIO DE LOGIN
+exports.login_get = (req, res, next) => {
+    res.render('login', {
+        title: 'Iniciar Sesión'
+    });
 }
 
-exports.login_post = async (req, res, next) => {
-    res.send("not implemented post")
-}
 
-exports.logout = async (req, res, next) => {
-    res.send("not implemented post logout")
-}
+// PROCESAR LOGIN (usa Passport con estrategia local)
+// =============================
+exports.login_post = (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/posts',
+        failureRedirect: '/auth/login',
+        failureFlash: true
+    })(req, res, next);
+};
 
-exports.register_get = async (req, res, next) => {
-    res.send("not implemented get register")
-}
 
+// LOGOUT
+// =================================
+exports.logout = (req, res, next) => {
+    // logout es fn de passport
+    req.logout((err) => {
+        if (err) {
+            return next(err)
+        }
+        req.flash('success_msg', 'Sesión cerrada exitosamente');
+        res.redirect('/auth/login');
+    });
+};
+
+
+
+// MOSTRAR FORMULARIO DE REGISTRO
+exports.register_get = (req, res, next) => {
+    res.render('register', {
+        title: "Registro de Usuario",
+        errors: [],
+    });
+};
+
+
+
+// PROCESAR REGISTRO
 exports.register_post = async (req, res, next) => {
-    res.send("not implemented gpost register")
-}
+    const { username, email, password, password2, descripcion } = req.body;
+    let errors = [];
+
+    // Validaciones
+    if (!username || !email || !password || !password2)
+        errors.push({ msg: 'Por favor complete todos los campos' });
+    if (password !== password2)
+        errors.push({ msg: 'Las dos contraseñas deben ser iguales' });
+    if (password < 6)
+        errors.push({ msg: 'La contraseña debe tener al menos 6 caracteres' });
+    if (username.length < 3)
+        errors.push({ msg: 'El nombre de usuario debe tener al menos 3 caracteres' });
+    if (descripcion && descripcion.length > 100)
+        errors.push({ msg: 'La descripcion no puede superar los 100 caracteres' });
+
+    // Si hay errores, volver a mostrar el formulario
+    if (errors.length > 0) {
+        return res.render('register', {
+            title: 'Registro de usuario',
+            errors,
+            username,
+            email,
+            descripcion,
+        });
+    }
+
+    // Verificar si el usuario ya existe, hay alguien con este email o este nombre de usuario?
+    try {
+        const existeUser = await User.findOne({
+            $or: [{ email: email }, { username: username }]
+        });
+
+        if (existeUser) {
+            errors.push({ msg: 'El email o nombre de usuario ya esta registrado' });
+            return res.render('register', {
+                title: 'Registro de usuario',
+                errors,
+                username,
+                email,
+                descripcion,
+            });
+        }
+
+        // HASHEAR CONTRASEÑA CON BCRYPT
+        const salt = await bcrypt.genSalt(10);
+
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // nuevo usuario 
+        const newUser = new User({
+            username: username,
+            email: email,
+            password: hashedPassword,
+            descripcion: descripcion || '',
+            isAdmin: false
+        });
+
+        // Guarda en MongoDB
+        await newUser.save();
+
+        req.flash('success_msg', 'Registro exitoso. Ahora puedes iniciar sesión');
+        res.redirect('/auth/login');
+
+
+    } catch (err) {
+        console.error(err);
+        errors.push({ msg: 'Error al registrar usuario' });
+        res.render('register', {
+            title: 'Registro de usuario',
+            errors,
+            username,
+            email,
+            descripcion
+        });
+    }
+
+};
