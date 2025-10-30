c95c2330259ff5eb56480c47a185f9b82233865e
marzvk <marianozovak1@gmail.com>
2025-10-30 16:21:12 -0300

    form editar comentarios, y controlador comentarios

--- controllers/comentarioControllers.js
+++ controllers/comentarioControllers.js
@@ -1,50 +1,178 @@
 
 const Comentario = require("../models/comentario");
 const Post = require("../models/post");
+const { body, validationResult } = require("express-validator");
 
 
-exports.create_post = async (req, res, next) => {
-    try {
-        // hasta implementar auth user 
-        const originalPost = await Post.findById(req.params.postId).exec();
+exports.create_post = [
+    body('content', 'El contenido es requerido')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('El comentario no puede estar vacio')
+        .isLength({ max: 200 })
+        .withMessage('El comentario no puede superar los 200 caracteres')
+        .escape(),
 
-        if (!originalPost) {
-            const err = new Error("Post no encontrado");
-            err.status = 404;
-            return next(err);
+    async (req, res, next) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // Usaremos flash para simplificar la redirección:
+            // Concatenamos todos los mensajes de error en un solo string
+            const errorMessages = errors.array().map(err => err.msg).join(', ');
+            req.flash('error_msg', errorMessages);
+
+            return res.redirect(`/posts/${req.params.postId}`);
         }
 
-        const { content } = req.body;
-        const { postId } = req.params;
-        const userId = req.user._id; // usuario logueado
+        try {
+            // hasta implementar auth user 
+            const originalPost = await Post.findById(req.params.postId).exec();
 
-        // instancia del comentario
-        const comentario = new Comentario({
-            content: content,
-            author: userId,
-            post: postId
+            if (!originalPost) {
+                const err = new Error("Post no encontrado");
+                err.status = 404;
+                return next(err);
+            }
+
+            const { content } = req.body;
+            const { postId } = req.params;
+            const userId = req.user._id; // usuario logueado
+
+            // instancia del comentario
+            const comentario = new Comentario({
+                content: content,
+                author: userId,
+                post: postId
+            });
+
+
+            await comentario.save();
+            res.redirect(`/posts/${postId}#comentario-${comentario._id}`);
+            // La redirección usa el ancla para saltar al nuevo comentario.
+
+            req.flash('success_msg', 'Comentario publicado exitosamente');
+
+
+        } catch (err) {
+            console.error(err);
+            req.flash('error_msg', 'Error al publicar el comentario');
+            res.redirect(`/posts/${req.params.postId}`);
+        }
+    }]
+
+//    UPDATE ******
+exports.update_get = async (req, res, next) => {
+    try {
+        const comentario = await Comentario.findById(req.params.comentarioId);
+
+
+        if (!comentario) {
+            req.flash('error_msg', 'Comentario no encontrado');
+            return res.redirect(`/posts/${req.params.postId}`);
+        }
+
+        if (comentario.author.toString() !== req.user._id.toString() && !req.user.isAdmin) {
+            req.flash('error_msg', 'No tienes permisos para editar este comentario');
+            return res.redirect(`/posts/${req.params.postId}`);
+        }
+
+        res.render('comentario_form', {
+            title: 'Editar Comentario',
+            post_id: req.params.postId,
+            comentario: comentario,
+            errors: []
         });
 
+    } catch (error) {
+        next(error);
+    }
+}
 
-        await comentario.save();
-        res.redirect(`/posts/${postId}#comentario-${comentario._id}`);
-        // La redirección usa el ancla para saltar al nuevo comentario.
+exports.update_post = [
+    body('content', 'El contenido es requerido')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('El comentario no puede estar vacio')
+        .isLength({ max: 200 })
+        .withMessage('El comentario no puede superar los 200 caracteres')
+        .escape(),
 
-    } catch (err) {
-        // Manejar errores de validación o DB
-        res.render('post_detail', {
-            title: 'Error',
-            error: err,
+    async (req, res, next) => {
+        const errors = validationResult(req);
 
-        });
+        if (!errors.isEmpty()) {
+            // Usaremos flash para simplificar la redirección:
+            // Concatenamos todos los mensajes de error en un solo string
+            comentarioInvalido.content = req.body.content;
+
+            res.render('comentario_form', {
+                title: 'Editar Comentario',
+                post_id: req.params.postId,
+                comentario: comentarioInvalido,
+                _id: comentarioId,// Pasamos el objeto con el texto inválido
+                errors: errors.array() // Pasamos el array de errores
+            });
+            return;
+        }
+
+        try {
+
+            const { content } = req.body;
+            const { postId, comentarioId } = req.params;
+            // const userId = req.user._id; // usuario logueado
+
+            const comentario = new Comentario({
+                content: content,
+                // author: userId,
+                post: postId,
+                _id: comentarioId,
+            });
+
+            const updatedComentario = await Comentario.findByIdAndUpdate(
+                comentarioId,
+                comentario,
+                {}
+            );
+
+            if (!updatedComentario) {
+                req.flash('error_msg', 'Comentario a actualizar no encontrado');
+                return res.redirect(`/posts/${postId}`);
+            }
+
+            req.flash('success_msg', 'Comentario actualizado exitosamente');
+            // Redirección al post, apuntando al comentario modificado
+            res.redirect(`/posts/${postId}#comentario-${comentarioId}`);
+
+        } catch (err) {
+            console.error(err);
+            req.flash('error_msg', 'Error al publicar el comentario');
+            res.redirect(`/posts/${req.params.postId}`);
+        }
+    }]
+
+
+//   DELETE  *****
+exports.delete_post = async (req, res, next) => {
+    try {
+        const comentario = await Comentario.findById(req.params.comentarioId);
+
+        if (!comentario) {
+            req.flash('error_msg', 'Comentario no encontrado');
+            return res.redirect('/posts');
+        }
+
+        // verificar que sea el autor o el admin
+        if (comentario.author.toString() !== req.user._id.toString() && !req.user.isAdmin) {
+            req.flash('error_msg', 'No tienes permisos para eliminar comentario')
+            return res.redirect(`/posts/${comentario.post}`);
+        }
+
+        await Comentario.findByIdAndDelete(req.params.comentarioId);
+        req.flash('success_msg', 'Comentario elimindo');
+        res.redirect(`/posts/${comentario.post}`);
+
+    } catch (error) {
+        return next(error);
     }
 };
-
-exports.update_put = async (req, res, next) => {
-    const { postId, comentarioId } = req.params;
-    const { content } = req.body;
-}
-
-exports.delete_delete = async (req, res, next) => {
-    const { postId, comentarioId } = req.params;
-}
